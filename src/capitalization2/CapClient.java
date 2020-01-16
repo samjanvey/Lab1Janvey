d76a912829b796ca4810647b40562c7e17bf9e75
Sam Janvey <sjanvey@gmail.com>
2020-01-15 21:41:33 -0500

    Added Comments and Exceptions

--- src/capitalization2/CapClient.java
+++ src/capitalization2/CapClient.java
@@ -1,4 +1,12 @@
-
+/* 
+Project: L01: Solo Work - A Capitalization Server
+Purpose Details: Set up Server/Client app that echoes text and capitalizes
+Course: IST411
+Author: Sam Janvey
+Date Developed: 1/15/2020
+Last Date Changed: 1/15/2020
+Revision: 
+*/
 package capitalization2;
 
 import java.io.BufferedReader;
@@ -12,33 +20,48 @@ import java.util.Scanner;
 public class CapClient {
     
     public static void main(String[] args) {
+        
+        // Print title to console
         System.out.println("Capitalization Client");
         
+        // Attempt to obtain local host address
         try {
             System.out.println("Waiting for connection......");
             InetAddress localAddress = InetAddress.getLocalHost();
             
+            // Attempt to connect to server on port 6000, establish channel for data exchange
             try (Socket clientSocket = new Socket(localAddress, 6000);
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 
+                // Success
                 System.out.println("Connected to server");
+                
+                // Get input from console
                 Scanner scanner = new Scanner(System.in);
+                
+                // Ask user for text input
+                // Continue prompting until user types "quit"
                 while(true) {
                     System.out.println("Enter text: ");
                     String inputLine = scanner.nextLine();
                     if("quit".equalsIgnoreCase(inputLine)) {
                         break;
                     }
+                    
+                    // Send text to server
                     out.println(inputLine);
+                    
+                    // Receive response from server and print to console
                     String response = br.readLine();
                     System.out.println("Server response: " + response);
                 }
-            
-            
             }
-        } catch (IOException ex) {
-            
+        } 
+        // Error Handling
+        catch (IOException ex) {
+            System.out.println("Oops, something went wrong.");
+            System.out.println("Try Again");
         }
     }
 }
--- src/capitalization2/CapServer.java
+++ src/capitalization2/CapServer.java
@@ -1,4 +1,12 @@
-
+/* 
+Project: L01: Solo Work - A Capitalization Server
+Purpose Details: Set up Server/Client app that echoes text and capitalizes
+Course: IST411
+Author: Sam Janvey
+Date Developed: 1/15/2020
+Last Date Changed: 1/15/2020
+Revision: 
+*/
 package capitalization2;
 
 import java.io.BufferedReader;
@@ -10,28 +18,42 @@ import java.net.Socket;
 
 public class CapServer {
     public static void main(String[] args) {
-        System.out.println("Capitalization Server");
         
+        // Print Title to the Console
+        System.out.println("Capitalization Server");
+
+        // Attempt to initiate server by listening on port 6000
         try (ServerSocket serverSocket = new ServerSocket(6000)) {
             System.out.println("Waiting for connection......");
+            
+            // Instantiate Socket object for client when incoming connection is made
             Socket clientSocket = serverSocket.accept();
             System.out.println("Connected to client");
             
+            // Attempt to establish I/O data channel for data exchange
             try (BufferedReader br = new BufferedReader(new InputStreamReader(
                     clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                
+
                 String inputLine;
+                
+                // Forever loop will run as long as data exists
+                // Input will be converted to Upper Case
                 while ((inputLine = br.readLine().toUpperCase()) != null) {
+                    
+                    // Echo back the input text from client
                     System.out.println("Server: " + inputLine);
+                    
+                    // Send text back to client
                     out.println(inputLine);
                 }
-            } 
-                
-        
+            }        
         }
+        // In case something goes wrong
+        // Print error message
         catch (IOException ex) {
-            
+            System.out.println("Oops, something went wrong.");
+            System.out.println("Try Again");
         }
         
     }
